169ab96d41120fc1bc0bbfd9aa6b0873e03169d8
Michael Martinez <rusalka2100@gmail.com>
2019-10-05 13:26:17 -0500

    Finish authorization

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import './NavBar.css';
 
-const NavBar = (props) => (
-    <nav className='NavBar'>
-        <h2>Title</h2>
-        <Link to='' className='NavBar-link'>Link 1</Link>
-        <Link to='' className='NavBar-link'>Link 2</Link>
-    </nav>
-
-);
+const NavBar = (props) => {
+    let nav = props.user ?
+      <div>
+        <Link to='' className='NavBar-link' onClick={props.handleLogout} >LOG OUT</Link>
+        &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+        <span className='NavBar-welcome'>WELCOME, {props.user.name}</span>
+      </div>
+      :
+      <div>
+        <Link to='/login' className='NavBar-link'>LOG IN</Link>
+        &nbsp;&nbsp;|&nbsp;&nbsp;
+        <Link to='/signup' className='NavBar-link'>SIGN UP</Link>
+      </div>;
+  
+    return (
+      <div className='NavBar'>
+        {nav}
+      </div>
+    );
+  };
 
 export default NavBar;
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,19 +1,54 @@
 import React, {Component} from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import './App.css';
+import userService from '../../utils/userService';
+import {Route, Switch} from 'react-router-dom';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      
+      user: userService.getUser()
     };
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
   }
 
   render() {
     return (
       <div>
-        <NavBar />
+        <Switch>
+          <Route exact path='/' render={() =>
+            <NavBar 
+              user={this.state.user}
+              handleLogout={this.handleLogout} 
+            />
+          }/>
+
+          <Route exact path='/signup' render={({ history }) => 
+              <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+
+          <Route exact path='/login' render={({history}) => 
+              <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+          }/>
+          
+        </Switch>
       </div>
     )
   }
