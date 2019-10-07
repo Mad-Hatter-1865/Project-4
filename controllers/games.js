88b3325d5d67c5e88925c3c114da6dea100e015f
Michael Martinez <rusalka2100@gmail.com>
2019-10-06 21:35:25 -0500

    Controllers functions

---
+++ controllers/games.js
@@ -0,0 +1,40 @@
+const Game = require('../models/game');
+
+module.exports = {
+    getAll,
+    createGame,
+    deleteGame,
+    editGame,
+    show
+};
+
+function getAll(req, res) {
+    Game.find({}).then(function(game) {
+        res.status(200).json(game);
+    });
+}
+
+function createGame(req, res) {
+    Game.create(req.body).then(function(game) {
+        res.status(201).json(game);
+    });
+}
+
+function deleteGame(req, res) {
+    Game.findByIdAndRemove(req.params.id).then(function(game) {
+      res.status(200).json(game);
+    });
+}
+
+function editGame(req, res) {
+    Game.findByIdAndUpdate(req.params.id, req.body, {new: true}).then(function(game) {
+      res.status(200).json(game);
+    });
+  }
+
+function show(req, res) {
+    Game.findById(req.params.id).then(function(game) {
+        res.status(200).json(game);
+    });
+}
+
